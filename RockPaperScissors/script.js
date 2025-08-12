6d2b8416db665d06cf46a20388d4963730d2af55
Sandesh-KC <kessysandesh111@gmail.com>
2025-08-12 13:59:41 +0545

    few adjustments

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -4,6 +4,38 @@ losses: 0,
 ties: 0
 };
 
+//using eventlistener instead of onclick
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('Rock')
+});
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('Paper')
+});
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('Scissors')
+});
+document.querySelector('.js-autoplay').addEventListener('click', () => {
+  autoPlay()
+});
+document.querySelector('.js-game-reset').addEventListener('click', () => {
+  resetConfirmation();
+});
+
+//feature to play using keys r,s, p and a keys
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('Rock');
+  } else if (event.key === 'p') {
+    playGame('Paper');
+  } else if (event.key === 's') {
+    playGame('Scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'Backspace') {
+    resetConfirmation();
+  }
+});
+
 //calling function to display score
 updateScoreElement ();
 
@@ -81,17 +113,47 @@ if (randomNumber >= 0 && randomNumber < 1 / 3) {
 return computerMove;
 }
 
+//reset score function
+function resetScore () {
+  score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement ();
+}
+
+//autoplay
 let autoPlaying = false;
 let intervalId;
 function autoPlay () {
   if (!autoPlaying) {
-    intervalId = setInterval(function() {
+    document.querySelector('.js-autoplay').innerHTML = 'Stop autoplaying';
+    intervalId = setInterval(() => {
       const playerMove = pickCompMove();
       playGame(playerMove);
     }, 1000);
     autoPlaying = true;
   } else {
+    document.querySelector('.js-autoplay').innerHTML = 'Autoplay';
     clearInterval(intervalId);
     intervalId = false;
+    autoPlaying = false;
   }
+}
+
+//reset confirmation
+function resetConfirmation () {
+  document.querySelector('.js-reset-confirmation').innerHTML = `Are you sure you want to reset the score? 
+  <button class="js-yes">Yes</button>
+  <button class="js-no">No</button>`
+
+  document.querySelector('.js-yes').addEventListener('click', () => {
+    resetScore();
+    resetHide();
+  });
+  document.querySelector('.js-no').addEventListener('click', () => resetHide());
+}
+
+function resetHide () {
+  document.querySelector('.js-reset-confirmation').innerHTML = '';
 }
